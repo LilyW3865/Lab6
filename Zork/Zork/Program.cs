1ba7b739ddf800610fad2d1e7abbc5260b77cb18
LilyW3865 <li733414@ucf.edu>
2025-09-29 17:00:08 -0400

    update

--- Zork/Zork/Program.cs
+++ Zork/Zork/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Zork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Zork!");
+
+            Commands command = Commands.UNKNOWN;
+
+            while(command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case "QUIT":
+                        command = command.QUIT;
+                        break;
+
+                    case "LOOK":
+                        command = command.LOOK;
+                        break;
+
+                    case "NORTH":
+                        command = command.NORTH;
+                        break;
+
+                    case "SOUTH":
+                        command = command.SOUTH;
+                        break;
+
+                    case "EAST":
+                        command = command.EAST;
+                        break;
+
+                    case "WEST":
+                        command = command.WEST;
+                        break;
+
+                    default:
+                        command = command.UNKNOWN;
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+
+            }
+        }
+
+        private static Commands ToCommand(string commandString) => (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
+
+    }
+}
